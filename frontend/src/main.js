35d47633a29c83b34854ba324b5a1d146b62ee26
soonpyojeong <103106668+soonpyojeong@users.noreply.github.com>
2025-04-11 16:10:20 +0900

    2025/04/11 1차배포 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
-
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://10.90.4.60:8813'; // Spring Boot 서버 URL
+
+createApp(App).use(router).mount('#app')
+
+
